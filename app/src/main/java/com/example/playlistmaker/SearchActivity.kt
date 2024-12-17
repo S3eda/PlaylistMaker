f475c151c2a43d24ae137b8457ca8df0cea2d3b5
maxim <maxim.kolotowckin@yandex.ru>
2024-12-17 17:29:31 +0300

    Верстка экрана "Поиск" и реализация посиковой строки при помощи EditText и TextWatcher

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,12 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +16,35 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val backImage = findViewById<ImageView>(R.id.back_search)
+        backImage.setOnClickListener{
+            finish()
+        }
+
+        val searchText = findViewById<EditText>(R.id.searchText)
+
+        val clearButton = findViewById<Button>(R.id.clearButton)
+        clearButton.setOnClickListener {
+            searchText.setText("")
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                clearButton.visibility = clearSearchButtonVisibility(s)
+            }
+        }
+        searchText.addTextChangedListener(searchTextWatcher)
+    }
+
+    private fun clearSearchButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
